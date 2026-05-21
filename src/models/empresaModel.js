66e812f5b6e2276a0849fb2b312a829db3ddd0bc
NicolasResr <npereirasantosdantas@gmail.com>
2026-05-20 21:56:34 -0300

    feat: adicionar estrutura inicial do backend Node.js

---
+++ src/models/empresaModel.js
@@ -0,0 +1,36 @@
+var database = require("../database/config");
+
+function buscarPorId(id) {
+  var instrucaoSql = `
+    SELECT * FROM Empresa 
+    WHERE idEmpresa = '${id}'`;
+  return database.executar(instrucaoSql);
+}
+
+function listar() {
+  var instrucaoSql = `
+    SELECT idEmpresa, razaoSocial, cnpj
+    FROM Empresa`;
+  return database.executar(instrucaoSql);
+}
+
+function buscarPorCnpj(cnpj) {
+  var instrucaoSql = `
+    SELECT * FROM Empresa 
+    WHERE cnpj = '${cnpj}'`;
+  return database.executar(instrucaoSql);
+}
+
+function cadastrar(razaoSocial, cnpj) {
+  var instrucaoSql = `
+    INSERT INTO Empresa (razaoSocial, cnpj) VALUES 
+    ( '${razaoSocial}', '${cnpj}')`;
+  return database.executar(instrucaoSql);
+}
+
+module.exports = {
+  buscarPorCnpj,
+  buscarPorId,
+  cadastrar,
+  listar
+};
