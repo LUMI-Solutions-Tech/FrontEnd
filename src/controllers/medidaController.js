f0a89ab15034f1a11a471e12088a076e976b5e43
Jonas Fernandes <diasjonas121@gmail.com>
2026-06-01 14:58:08 -0300

    redirecionando arquivo de dash-geral para public

--- src/controllers/medidaController.js
+++ src/controllers/medidaController.js
@@ -2,13 +2,13 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
+    const limite_linhas = 1;
 
-    var idAquario = req.params.idAquario;
+    var idSensor = req.params.idSensor;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idSensor, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -22,27 +22,27 @@ function buscarUltimasMedidas(req, res) {
 }
 
 
-function buscarMedidasEmTempoReal(req, res) {
+// function buscarMedidasEmTempoReal(req, res) {
 
-    var idAquario = req.params.idAquario;
+//     var idSensor = req.params.idSensor;
 
-    console.log(`Recuperando medidas em tempo real`);
+//     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
-}
+//     medidaModel.buscarMedidasEmTempoReal(idSensor).then(function (resultado) {
+//         if (resultado.length > 0) {
+//             res.status(200).json(resultado);
+//         } else {
+//             res.status(204).send("Nenhum resultado encontrado!")
+//         }
+//     }).catch(function (erro) {
+//         console.log(erro);
+//         console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+//         res.status(500).json(erro.sqlMessage);
+//     });
+// }
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+//  buscarMedidasEmTempoReal
 
 }
--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,35 +1,32 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idSensor, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
+        ppfd 
+                    FROM leituras
+                    WHERE fksensor = ${idSensor}
                     ORDER BY id DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+// function buscarMedidasEmTempoReal(idAquario) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+//     var instrucaoSql = `SELECT 
+//         dht11_temperatura as temperatura, 
+//         dht11_umidade as umidade,
+//                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+//                         fk_aquario 
+//                         FROM medida WHERE fk_aquario = ${idAquario} 
+//                     ORDER BY id DESC LI'MIT 1`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
+//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//     return database.executar(instrucaoSql);
+// }
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    // buscarMedidasEmTempoReal
 }
